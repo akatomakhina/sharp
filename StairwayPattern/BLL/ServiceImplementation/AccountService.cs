381a82232bffa42122151dc23c7e6ce95d5622ed
Anastasiya Katomakhina <katomakhinaanastasiya@gmail.com>
2017-11-15 22:33:00 +0300

    project added

---
+++ StairwayPattern/BLL/ServiceImplementation/AccountService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BLL.Interface.Entities;
+using BLL.Interface.Interfaces;
+using BLL.Mappers;
+using DAL.Interface.Interfaces;
+
+namespace BLL.ServiceImplementation
+{
+    public class AccountService : IAccountService
+    {
+        private IRepository repository;
+
+        public AccountService(IRepository repository) 
+        {
+            this.repository = repository;
+        }
+
+        public void OpenAccount(string name, AccountType accountType, IAccountNumberCreateService creator)
+        {
+            string accountNumber = creator.Create(GetNumberOfAccounts());
+
+            Type type = Type.GetType($"BLL.Interface.Entities.{accountType}Account,BLL.Interface");//??
+
+            Account account = (Account)Activator.CreateInstance(type, new object[] { accountNumber, name });
+
+            // Account account = null;
+            //switch (accountType)
+            //{
+            //    case AccountType.Base:
+            //        account = new BaseAccount(accountNumber, name);
+            //        break;
+            //    case AccountType.Silver:
+            //        account = new SilverAccount(accountNumber, name);
+            //        break;
+            //}
+
+            repository.Create(account.ToAccountDto());
+        }
+
+        public void DepositAccount(string accountNumber, decimal amount)
+        {
+            Account account = repository.GetByNumber(accountNumber).ToAccount();
+
+            account.Deposit(amount);
+
+            repository.Update(account.ToAccountDto());
+        }
+
+        public void WithdrawAccount(string accountNumber, decimal amount)
+        {
+            Account account = repository.GetByNumber(accountNumber).ToAccount();
+
+            account.Whithdraw(amount);
+
+            repository.Update(account.ToAccountDto());
+        }
+
+        public IEnumerable<Account> GetAllAccounts() => repository.GetAllAccounts().Select(item => item.ToAccount());
+
+        private int GetNumberOfAccounts() => GetAllAccounts().ToList().Count;
+    }
+}
---
+++ StairwayPattern/ConsolePL/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using BLL.Interface.Entities;
+using BLL.Interface.Interfaces;
+using BLL.ServiceImplementation;
+
+namespace ConsolePL
+{
+    class Program
+    {
+
+        static Program()
+        {
+        }
+
+        static void Main(string[] args)
+        {
+            IAccountService service = new AccountService(IRepository());//??
+            IAccountNumberCreateService creator = new AccountNumberCreator();
+
+            service.OpenAccount("Account owner 1", AccountType.Base, creator);
+            service.OpenAccount("Account owner 2", AccountType.Base, creator);
+            service.OpenAccount("Account owner 3", AccountType.Silver, creator);
+            service.OpenAccount("Account owner 4", AccountType.Base, creator);
+
+            var creditNumbers = service.GetAllAccounts().Select(acc => acc.AccountNumber).ToArray();
+
+            foreach (var t in creditNumbers)
+            {
+                service.DepositAccount(t, 100);
+            }
+
+            foreach (var item in service.GetAllAccounts())
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var t in creditNumbers)
+            {
+                service.WithdrawAccount(t, 10);
+            }
+
+            foreach (var item in service.GetAllAccounts())
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
